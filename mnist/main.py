0272b929477f6996c51692042e6bcd76a5e19dca
minghao <minghao1223@gmail.com>
2019-11-15 15:14:51 +0900

    Now it prints hi

--- mnist/main.py
+++ mnist/main.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
-
+print('Hi')
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
